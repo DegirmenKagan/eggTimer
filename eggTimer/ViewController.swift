43da7b99191d59646e5ce7e9ff4d6db8ead701b1
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-25 22:55:47 +0300

    playing alarm sound, button title coloring

--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -19,6 +20,25 @@ class ViewController: UIViewController {
     var totalTime = 0
     var secondsPassed = 0
     var timer = Timer()
+    var player = AVAudioPlayer()
+    
+    @IBAction func eggSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        titleLabel.text = hardness
+        secondsPassed = 0
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        
+        // Coloring selected egg, first resetting all then coloring selected.
+        softButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        mediumButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        hardButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        sender.setTitleColor(UIColor.systemYellow, for: UIControl.State.normal)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
     
     @objc func updateCounter() {
         if secondsPassed < totalTime {
@@ -29,20 +49,17 @@ class ViewController: UIViewController {
             timer.invalidate()
             titleLabel.text = "Done!"
             softButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+            mediumButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+            hardButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+            playAlarmSound()
         }
             
     }
-
-    @IBAction func eggSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
-        titleLabel.text = hardness
-        sender.setTitleColor(UIColor.systemYellow, for: UIControl.State.normal)
-        secondsPassed = 0
-        totalTime = eggTimes[hardness]!
-        progressBar.progress = 0.0
-        
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-        
+    
+    func playAlarmSound(){
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
     
 }
