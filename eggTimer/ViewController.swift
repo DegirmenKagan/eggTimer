d113d60b728086db98809fc601383ec62560bede
Kağan Değirmen <kagandegirmen@gmail.com>
2023-05-25 22:32:56 +0300

    button functionality added, timer implemented

--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var softButton: UIButton!
+    @IBOutlet weak var mediumButton: UIButton!
+    @IBOutlet weak var hardButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes = ["Soft": 300,"Medium":420,"Hard":720]
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
+    
+    @objc func updateCounter() {
+        if secondsPassed < totalTime {
+            secondsPassed+=1
+            
+            
+        }else{
+            timer.invalidate()
+            titleLabel.text = "Done!"
+            softButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        }
+            
     }
 
-
+    @IBAction func eggSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        titleLabel.text = hardness
+        sender.setTitleColor(UIColor.systemYellow, for: UIControl.State.normal)
+        secondsPassed = 0
+        totalTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+    
 }
 
